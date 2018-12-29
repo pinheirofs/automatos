8864140e03d36148576fdd194053ec52c2c2371c
Felipe Santos Pinheiro <pinheirofs@gmail.com>
2018-12-29 16:43:35 -0200

    inclusao da exibicao do exemplo de janela 3D

--- simArm/main.cpp
+++ simArm/main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+#include <string>
+
+#include <Qt3DQuickExtras/Qt3DQuickWindow>
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
 #include <actuator.hpp>
+
+using std::cerr;
+using std::endl;
 
 int main(int argc, char *argv[]) {
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -12,8 +20,14 @@ int main(int argc, char *argv[]) {
 
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-  if (engine.rootObjects().isEmpty())
+  if (engine.rootObjects().isEmpty()) {
+    cerr << "Erro ao carregar tela de controles" << endl;
     return -1;
+  }
+
+  Qt3DExtras::Quick::Qt3DQuickWindow view;
+  view.setSource(QUrl(QStringLiteral("qrc:/arm3d.qml")));
+  view.show();
 
   return app.exec();
 }
